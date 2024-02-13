97321b8675fe3c4707c81cff459b9cd5867efb0d
sashwat tomar <85127093+sashwattomar@users.noreply.github.com>
2024-02-13 11:04:08 +0530

    Time: 66 ms (48.24%) | Memory: 50 MB (26.38%) - LeetSync

---
+++ 2741-function-composition/function-composition.js
@@ -0,0 +1,20 @@
+/**
+ * @param {Function[]} functions
+ * @return {Function}
+ */
+var compose = function(functions) {
+    
+    return function(x) {
+        
+    return  functions.reverse().reduce((acc,item)=>{
+            acc = item(acc);
+            return acc;
+        },x)
+
+    }
+};
+
+/**
+ * const fn = compose([x => x + 1, x => 2 * x])
+ * fn(4) // 9
+ */
