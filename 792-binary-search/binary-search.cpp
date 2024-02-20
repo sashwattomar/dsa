0c645c4bb0b39f59d231fa1ccbba75971b95fef6
sashwat tomar <85127093+sashwattomar@users.noreply.github.com>
2024-02-20 21:46:07 +0530

    Time: 23 ms (87.26%) | Memory: 29.7 MB (48.73%) - LeetSync

---
+++ 792-binary-search/binary-search.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int binarySearch(vector<int>& nums, int target, int si , int ei) {
+    
+        if( si>ei ){
+            return -1;
+        }
+
+
+    long long mid  = si+((ei-si)%2);
+
+    if( nums[mid] == target ) {
+        return mid;
+    }
+    if( nums[mid] > target ) {
+        return binarySearch( nums, target, si, mid-1);
+    } else {
+        return binarySearch( nums, target, mid+1, ei);
+    }
+
+    } 
+
+    int search(vector<int>& nums, int target) {
+      return  binarySearch(nums, target, 0, nums.size()-1);
+    }
+};
